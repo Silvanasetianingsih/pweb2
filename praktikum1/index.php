0152e721239d892e447e0ae8b5fde19a2f341ffe
Silvanasetianingsih <145969826+Silvanasetianingsih@users.noreply.github.com>
2024-03-01 21:36:21 +1400

    Add files via upload

---
+++ praktikum1/index.php
@@ -0,0 +1,10 @@
+<?php
+// Membuat variabel user
+$name ="silvanasetia ningsih" ;
+$age = 19;
+$weight  = 53;
+
+echo $name;
+
+
+?>
---
+++ praktikum1/submit)_get.php
@@ -0,0 +1,22 @@
+<body>
+    <hi>from Registrasi pengguna</hi>
+    <from action="submit_get.php"method="GET">
+        <div>
+            <label>.for="nama lengkap">Nama.lengkap</label>
+            <input type="text" name="nama-lengkap" id="nama-lengkap"required>
+            </div><br>
+            <div>
+                <label> for="usename">Username</label>
+                <input> type="text".name="username" id="username"required>
+</div><br>
+<div>
+    <label.for="password">password</label>
+    <input.type="password".name="password".id="password".required>
+    <div><br>
+        <div>
+            <bubtton.type="submit">Register</button>
+        </div>
+    </from>
+    </body>
+
+    </html>
